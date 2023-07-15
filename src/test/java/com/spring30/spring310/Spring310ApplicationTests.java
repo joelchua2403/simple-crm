1f7b88f07a1a4758f5c7b7dc72608ccf65c1770a
Joel Chua <joel.chua.mingyi@gmail.com>
2023-07-15 12:12:51 +0800

    change to h2db

--- src/test/java/com/spring30/spring310/Spring310ApplicationTests.java
+++ src/test/java/com/spring30/spring310/Spring310ApplicationTests.java
@@ -4,7 +4,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.mockito.Mock;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.autoconfigure.graphql.tester.AutoConfigureGraphQlTester;
+
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.web.servlet.MockMvc;
